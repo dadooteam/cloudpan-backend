8633cdbf25de288958ec5a50d50e6f4ac95b6cec
dadooteam <dadooteam>
2017-01-06 17:27:56 +0800

    完成download和upload接口

--- src/main/java/im/dadoo/cloudpan/backend/controller/FileController.java
+++ src/main/java/im/dadoo/cloudpan/backend/controller/FileController.java
@@ -4,16 +4,26 @@ import com.google.gson.Gson;
 import im.dadoo.cloudpan.backend.dto.FileDto;
 import im.dadoo.cloudpan.backend.dto.Result;
 import im.dadoo.cloudpan.backend.so.FileSo;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -43,4 +53,45 @@ public class FileController {
         SLOGGER.info(this.gson.toJson(logMap));
         return r;
     }
+
+    @RequestMapping(value = "/download", method = RequestMethod.GET)
+    @ResponseBody
+    public Result<?> download(HttpServletRequest request, HttpServletResponse response) {
+        String path = request.getParameter("path");
+        Result<File> r = this.fileSo.download(path);
+        File file = r.getData();
+        if (file != null) {
+            try {
+                InputStream is = new BufferedInputStream(new FileInputStream(file));
+                String mimeType = URLConnection.guessContentTypeFromStream(is);
+                if (StringUtils.isBlank(mimeType)){
+                    mimeType = "application/octet-stream";
+                }
+                response.setContentType(mimeType);
+                response.setHeader(HttpHeaders.CONTENT_DISPOSITION, String.format("inline; filename=\"" + file.getName() +"\""));
+                response.setContentLength((int)file.length());
+                FileCopyUtils.copy(is, response.getOutputStream());
+            } catch (Exception e) {}
+        }
+
+        Map<String, Object> logMap = new HashMap<>();
+        logMap.put("path", path);
+        SLOGGER.info(this.gson.toJson(logMap));
+        return r;
+    }
+
+  @RequestMapping(value = "/upload", method = RequestMethod.POST)
+  @ResponseBody
+  public Result<?> upload(MultipartHttpServletRequest request, HttpServletResponse response) {
+    String path = request.getParameter("path");
+    MultipartFile file = request.getFile("file");
+    Result<Object> r = this.fileSo.upload(path, file);
+
+    Map<String, Object> logMap = new HashMap<>();
+    logMap.put("path", path);
+    logMap.put("filename", file.getName());
+    SLOGGER.info(this.gson.toJson(logMap));
+
+    return r;
+  }
 }
--- src/main/java/im/dadoo/cloudpan/backend/so/FileSo.java
+++ src/main/java/im/dadoo/cloudpan/backend/so/FileSo.java
@@ -1,6 +1,5 @@
 package im.dadoo.cloudpan.backend.so;
 
-import com.google.common.io.Files;
 import im.dadoo.cloudpan.backend.dto.FileDto;
 import im.dadoo.cloudpan.backend.dto.Result;
 import org.apache.commons.lang3.StringUtils;
@@ -8,9 +7,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,7 +21,6 @@ import java.util.stream.Collectors;
 /**
  * Created by codekitten on 2016/12/27.
  */
-
 @Service
 public class FileSo {
 
@@ -32,7 +34,7 @@ public class FileSo {
         r.setCode(500_001_000);
         try {
             if (StringUtils.isNotBlank(path)) {
-                String fullpath = this.env.getProperty("master.path") + path;
+                String fullpath = this.env.getProperty("master.path") + "/" +path;
                 File fullFile = new File(fullpath);
                 if (fullFile != null && fullFile.exists()) {
                 List<FileDto> data = Arrays.asList(fullFile.listFiles()).stream().map(file -> {
@@ -46,9 +48,49 @@ public class FileSo {
             }
             r.setCode(200_001_000);
         } catch (Exception e) {
-
+            ELOGGER.error(String.format("%s遍历失败", path), e);
         }
         return r;
+    }
+
+    public Result<File> download(String path) {
+        Result<File> r = new Result<>();
+        r.setCode(500_001_000);
+        try {
+            if (StringUtils.isNotBlank(path)) {
+                String fullpath = this.env.getProperty("master.path") + "/" + path;
+                File fullFile = new File(fullpath);
+                if (fullFile != null && fullFile.exists() && fullFile.isFile()) {
+                    r.setData(fullFile);
+                }
+            }
+            r.setCode(200_001_000);
+        } catch (Exception e) {
+            ELOGGER.error(String.format("%s下载失败", path), e);
+        }
+        return r;
+    }
+
+    public Result<Object> upload(String path, MultipartFile file) {
+      Result<Object> r = new Result<>();
+      r.setCode(500_001_000);
+      try {
+        String fullpath = this.env.getProperty("master.path") + "/" + path + "/" + file.getName();
+        File newFile = new File(fullpath);
+        if (newFile.exists()) {
+          newFile.delete();
+        }
+        if (newFile.createNewFile()) {
+          FileCopyUtils.copy(file.getInputStream(), new BufferedOutputStream(new FileOutputStream(newFile)));
+        } else {
+          throw new Exception(String.format("创建新文件失败，路径%s，文件名%s", fullpath, file.getName()));
+        }
+        r.setCode(200_001_000);
+      } catch (Exception e) {
+        r.setMessage(e.getLocalizedMessage());
+        ELOGGER.error(Long.toString(r.getCode()), e);
+      }
+      return r;
     }
 
 }
